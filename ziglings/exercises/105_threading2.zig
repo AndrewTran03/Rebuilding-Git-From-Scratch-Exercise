33c410b1cd5249b6732d50c20f991f1c19232c48
Andrew Tran <andrewtranva@gmail.com>
2025-06-16 17:40:20 -0400

    Finished Zig Practice Exercise 105

--- ziglings/exercises/105_threading2.zig
+++ ziglings/exercises/105_threading2.zig
@@ -70,29 +70,37 @@
 //
 const std = @import("std");
 
+const Operation = enum(i8) {
+    ADD,
+    SUBTRACT
+};
+
 pub fn main() !void {
-    const count = 1_000_000_000;
+    const count = 10_000_000_000;
     var pi_plus: f64 = 0;
     var pi_minus: f64 = 0;
 
     {
         // First thread to calculate the plus numbers.
-        const handle1 = try std.Thread.spawn(.{}, thread_pi, .{ &pi_plus, 5, count });
+        const handle1 = try std.Thread.spawn(.{}, thread_pi, .{ &pi_plus, 5, count, Operation.ADD });
         defer handle1.join();
 
         // Second thread to calculate the minus numbers.
-        ???
-        
+        const handle2 = try std.Thread.spawn(.{}, thread_pi, .{ &pi_minus, 7, count, Operation.SUBTRACT });
+        defer handle2.join();
     }
     // Here we add up the results.
-    std.debug.print("PI ≈ {d:.8}\n", .{4 + pi_plus - pi_minus});
+    std.debug.print("PI ≈ {d:.8}\n", .{4 + pi_plus + pi_minus});
 }
 
-fn thread_pi(pi: *f64, begin: u64, end: u64) !void {
+fn thread_pi(pi: *f64, begin: u64, end: u64, sign: Operation) !void {
+    var local_sum: f64 = 0;
     var n: u64 = begin;
+    const multiplier: f64 = if (sign == Operation.ADD) 1.0 else -1.0;
     while (n < end) : (n += 4) {
-        pi.* += 4 / @as(f64, @floatFromInt(n));
+        local_sum += multiplier * 4 / @as(f64, @floatFromInt(n));
     }
+    pi.* = local_sum;
 }
 // If you wish, you can increase the number of loop passes, which
 // improves the number of digits.
